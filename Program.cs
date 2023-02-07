75b650e2274487905705858eb1e847323a8aaab8
Jirayu_SBTR <jirayu.majesty@mail.kmutt.ac.th>
2023-02-07 20:12:57 +0700

    F(x)101

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+class Program
+{
+    static void Main (string[] args)
+    {
+        double x = 0;
+        x = double.Parse(Console.ReadLine());
+        Console.WriteLine(F(x));
+    }
+    static double F (double x)
+    {
+        if ( x > 4 )
+        {
+            return 1+F(x-((4*Math.Abs(x))/x));
+        }
+        else
+        {
+            return 2;
+        }
+    }
+}
